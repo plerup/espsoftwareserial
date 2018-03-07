c01bb2d2955a8dccee79e7e0864258202ae4b44a
Dirk O. Kaar <dok@dok-net.net>
2018-03-08 00:30:43 +0100

    repeater example, report mismatch errors in sequential values.

--- examples/repeater/repeater.ino
+++ examples/repeater/repeater.ino
@@ -4,6 +4,8 @@ SoftwareSerial repeater(D5, D6);
 unsigned long start;
 String bitRateTxt("\tBitrate: ");
 int rxCount;
+int seqErrors;
+int expected;
 constexpr int ReportInterval = 100000;
 
 void setup()
@@ -13,6 +15,8 @@ void setup()
 	repeater.enableIntTx(true);
 	start = micros();
 	rxCount = 0;
+	seqErrors = 0;
+	expected = -1;
 }
 
 void loop()
@@ -20,6 +24,12 @@ void loop()
 	while (repeater.available())
 	{
 		int r = repeater.read();
+		if (expected == -1) expected = r;
+		else
+		{
+			expected = ++expected % 256;
+		}
+		if (r != expected) ++seqErrors;
 		++rxCount;
 		repeater.write(r);
 		if (rxCount >= ReportInterval) break;
@@ -28,8 +38,10 @@ void loop()
 	if (rxCount >= ReportInterval) {
 		auto end = micros();
 		auto cps = 1000000 / ((end - start) / rxCount);
-		Serial.println(bitRateTxt + 10 * cps + "bps");
+		Serial.println(bitRateTxt + 10 * cps + "bps, " + seqErrors / (end - start) + "seq. errors/s");
 		start = end;
 		rxCount = 0;
+		expected = -1;
+		seqErrors = 0;
 	}
 }
