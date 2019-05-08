07c03d9cf1a795dd00a52da1ee52abd44b6b5804
Dirk O. Kaar <dok@dok-net.net>
2019-05-08 16:10:20 +0200

    Lock-free for multi-producer / single-consumer.

--- src/CircularQueue.h
+++ src/CircularQueue.h
@@ -30,7 +30,9 @@ public:
 	CircularQueue() = delete;
 	CircularQueue(size_t capacity) : m_bufSize(capacity + 1), m_buffer(new std::atomic<T>[m_bufSize])
 	{
-		m_inPos.store(0);
+		m_inPosL.store(0);
+		m_inPosC.store(0);
+		m_inPosT.store(0);
 		m_outPos.store(0);
 	}
 	~CircularQueue()
@@ -41,12 +43,12 @@ public:
 	CircularQueue& operator=(const CircularQueue&) = delete;
 
 	void flush() {
-		m_outPos.store(m_inPos.load());
+		m_outPos.store(m_inPosT.load());
 	}
 
 	size_t available()
 	{
-		ssize_t avail = m_inPos.load() - m_outPos.load();
+		ssize_t avail = m_inPosT.load() - m_outPos.load();
 		if (avail < 0) avail += m_bufSize;
 		return avail;
 	}
@@ -54,25 +56,45 @@ public:
 	T peek()
 	{
 		auto outPos = m_outPos.load();
-		return (m_inPos.load() == outPos) ? defaultValue : m_buffer[outPos].load();
+		return (m_inPosT.load() == outPos) ? defaultValue : m_buffer[outPos].load();
 	}
 
 	bool ICACHE_RAM_ATTR push(T val)
+#ifdef ESP8266
 	{
-		auto inPos = m_inPos.load();
+		uint32_t savedPS = xt_rsil(15);
+		auto inPos = m_inPosT.load();
 		int next = (inPos + 1) % m_bufSize;
-		if (next != m_outPos.load()) {
-			m_buffer[inPos].store(val);
-			m_inPos.store(next);
-			return true;
-		}
-		return false;
+		if (next == m_outPos.load()) return false;
+		m_inPosL.store(next);
+
+		m_buffer[inPos].store(val);
+
+		xt_wsr_ps(savedPS);
+		return true;
 	}
+#else
+	{
+		int nextL;
+		auto inPos = m_inPosL.load();
+		do {
+			nextL = (inPos + 1) % m_bufSize;
+			if (nextL == m_outPos.load()) return false;
+		} while (!m_inPosL.compare_exchange_weak(inPos, nextL));
+
+		m_buffer[inPos].store(val);
+
+		int nextC = ++m_inPosC % m_bufSize;
+		if (nextC == m_inPosL.load()) m_inPosT.store(nextC);
+
+		return true;
+	}
+#endif
 
 	T pop()
 	{
 		auto outPos = m_outPos.load();
-		if (m_inPos.load() == outPos) return defaultValue;
+		if (m_inPosT.load() == outPos) return defaultValue;
 		auto val = m_buffer[outPos].load();
 		m_outPos.store((outPos + 1) % m_bufSize);
 		return val;
@@ -96,7 +118,9 @@ protected:
 	const T defaultValue = {};
 	int m_bufSize;
 	std::unique_ptr<std::atomic<T>[] > m_buffer;
-	std::atomic<int> m_inPos;
+	std::atomic<int> m_inPosL;
+	std::atomic<int> m_inPosC;
+	std::atomic<int> m_inPosT;
 	std::atomic<int> m_outPos;
 };
 
