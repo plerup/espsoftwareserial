84e5b15b6fdda0294c2d07c08968bfa086436897
Dirk O. Kaar <dok@dok-net.net>
2019-06-21 10:26:12 +0200

    Cleanup includes in response to fixed included in cores/esp8266/interrupts.h

--- src/SoftwareSerial.cpp
+++ src/SoftwareSerial.cpp
@@ -20,9 +20,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
-#include <Arduino.h>
-
 #include <SoftwareSerial.h>
+#include <Arduino.h>
 
 #ifdef ESP32
 #define xt_rsil(a) (a)
