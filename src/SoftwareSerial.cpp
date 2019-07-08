584e689550d32bbcfb6b7d0b7d37b42fba30c4bf
Dirk O. Kaar <dok@dok-net.net>
2019-07-08 14:51:10 +0200

    Drop redundant include

--- src/SoftwareSerial.cpp
+++ src/SoftwareSerial.cpp
@@ -21,9 +21,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include <SoftwareSerial.h>
-#ifdef ESP8266
-#include <FunctionalInterrupt.h>
-#endif
 #include <Arduino.h>
 
 #ifdef ESP32
