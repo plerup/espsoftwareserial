f40227853273106ae1d5c658cba3660763de964b
Dirk O. Kaar <dok@dok-net.net>
2019-07-24 18:11:21 +0200

    Performance optimization: for_each_rev_requeue, >10%

--- src/circular_queue/circular_queue_mp.h
+++ src/circular_queue/circular_queue_mp.h
@@ -127,7 +127,15 @@ public:
                 Requeuing is dependent on the return boolean of the callback function. If it
                 returns true, the requeue occurs.
     */
-    bool for_each_requeue(std::function<bool(T&)> fun);
+    bool for_each_requeue(const std::function<bool(T&)>& fun);
+
+    /*!
+        @brief	In reverse order, iterate over, pop and optionally requeue each available element from the queue,
+                calling back fun with a reference of every single element.
+                Requeuing is dependent on the return boolean of the callback function. If it
+                returns true, the requeue occurs.
+    */
+    bool for_each_rev_requeue(const std::function<bool(T&)>& fun);
 
 #ifndef ESP8266
 protected:
@@ -156,14 +164,14 @@ T& circular_queue_mp<T>::pop_requeue()
 }
 
 template< typename T >
-bool circular_queue_mp<T>::for_each_requeue(std::function<bool(T&)> fun)
+bool circular_queue_mp<T>::for_each_requeue(const std::function<bool(T&)>& fun)
 {
     auto inPos0 = circular_queue<T>::m_inPos.load(std::memory_order_acquire);
     auto outPos = circular_queue<T>::m_outPos.load(std::memory_order_relaxed);
     std::atomic_thread_fence(std::memory_order_acquire);
     if (outPos == inPos0) return false;
     do {
-        T& val = circular_queue<T>::m_buffer[outPos];
+        T&& val = std::move(circular_queue<T>::m_buffer[outPos]);
         if (fun(val))
         {
 #ifdef ESP8266
@@ -185,6 +193,29 @@ bool circular_queue_mp<T>::for_each_requeue(std::function<bool(T&)> fun)
         outPos = (outPos + 1) % circular_queue<T>::m_bufSize;
         circular_queue<T>::m_outPos.store(outPos, std::memory_order_release);
     } while (outPos != inPos0);
+    return true;
+}
+
+template< typename T >
+bool circular_queue_mp<T>::for_each_rev_requeue(const std::function<bool(T&)>& fun)
+{
+    auto inPos0 = circular_queue<T>::m_inPos.load(std::memory_order_acquire);
+    auto outPos = circular_queue<T>::m_outPos.load(std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_acquire);
+    if (outPos == inPos0) return false;
+    auto pos = inPos0;
+    auto outPos1 = inPos0;
+	const auto posDecr = circular_queue<T>::m_bufSize - 1;
+    do {
+        pos = (pos + posDecr) % circular_queue<T>::m_bufSize;
+        T&& val = std::move(circular_queue<T>::m_buffer[pos]);
+        if (fun(val))
+        {
+            outPos1 = (outPos1 + posDecr) % circular_queue<T>::m_bufSize;
+            if (outPos1 != pos) circular_queue<T>::m_buffer[outPos1] = std::move(val);
+        }
+    } while (pos != outPos);
+    circular_queue<T>::m_outPos.store(outPos1, std::memory_order_release);
     return true;
 }
 
