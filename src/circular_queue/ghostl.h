8ea4784f80e82bfc85c9a43bd6586a92b3a6e473
Dirk O. Kaar <dok@dok-net.net>
2023-07-15 23:06:07 +0200

    Fix ghostl for ESP8266

--- src/circular_queue/ghostl.h
+++ src/circular_queue/ghostl.h
@@ -30,21 +30,25 @@ using size_t = decltype(sizeof(char));
 
 namespace std
 {
-#if !defined(ARDUINO_ARCH_SAMD)
+#if !defined(ARDUINO_ARCH_SAMD) && !defined(ESP8266)
     typedef enum memory_order {
         memory_order_relaxed,
         memory_order_acquire,
         memory_order_release,
         memory_order_seq_cst
     } memory_order;
+
     template< typename T > class atomic {
     private:
         T value;
     public:
         atomic() {}
         atomic(T desired) { value = desired; }
+
         void store(T desired, std::memory_order = std::memory_order_seq_cst) volatile noexcept { value = desired; }
+
         T load(std::memory_order = std::memory_order_seq_cst) const volatile noexcept { return value; }
+
         T exchange(T desired, std::memory_order = std::memory_order_seq_cst) const volatile noexcept {
             noInterrupts();
             T orig = value;
@@ -52,6 +56,7 @@ namespace std
             interrupts();
             return orig;
         }
+
         bool compare_exchange_strong(T& expected, T desired, std::memory_order order = std::memory_order_seq_cst) volatile noexcept {
             noInterrupts();
             const bool equal = value == expected;
@@ -60,14 +65,18 @@ namespace std
             interrupts();
             return equal;
         }
+
         bool compare_exchange_weak(T& expected, T desired, std::memory_order order = std::memory_order_seq_cst) volatile noexcept {
             return compare_exchange_strong(expected, desired, order);
         };
     };
+
     inline void atomic_thread_fence(std::memory_order order) noexcept {}
+
     template< typename T > T&& move(T& t) noexcept { return static_cast<T&&>(t); }
 #endif
 
+#ifndef ESP8266
     template< typename T, size_t long N > struct array
     {
         T _M_elems[N];
@@ -107,6 +116,58 @@ namespace std
     {
         return static_cast<typename identity<T>::type&&>(t);
     }
-}
+#endif // ESP8266
 
+#ifdef ESP8266
+#if defined (__cplusplus)
+    extern "C" {
+#endif
+        bool __atomic_compare_exchange_1(uint8_t* ptr, uint8_t* expected, uint8_t desired,
+            bool weak, int success_memorder, int failure_memorder)
+        {
+            (void)weak;
+            (void)success_memorder;
+            (void)failure_memorder;
+            noInterrupts();
+            const bool equal = *ptr == *expected;
+            if (equal) *ptr = desired;
+            else *expected = *ptr;
+            interrupts();
+            return equal;
+        }
+
+        uint8_t __atomic_exchange_1(uint8_t* ptr, uint8_t value, int memorder)
+        {
+            (void)memorder;
+            noInterrupts();
+            uint8_t orig = *ptr;
+            *ptr = value;
+            interrupts();
+            return orig;
+        }
+
+        uint32_t __atomic_fetch_add_4(uint32_t* ptr, uint32_t value, int memorder)
+        {
+            (void)memorder;
+            noInterrupts();
+            uint32_t orig = *ptr;
+            *ptr += value;
+            interrupts();
+            return orig;
+        }
+
+        uint32_t __atomic_fetch_sub_4(uint32_t* ptr, uint32_t value, int memorder)
+        {
+            (void)memorder;
+            noInterrupts();
+            uint32_t orig = *ptr;
+            *ptr -= value;
+            interrupts();
+            return orig;
+        }
+#if defined (__cplusplus)
+    } // extern "C"
+#endif
+#endif // ESP8266
+}
 #endif // __ghostl_h
